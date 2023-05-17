efce5b16cff412047741555fcf13212c1ab9c95c
SC0011 <monte.faison@seattlecolleges.edu>
2023-05-17 11:22:25 -0700

    1010 class

--- button1010.py
+++ button1010.py
@@ -1,12 +1,18 @@
 # 
 import tkinter as tk
 
-
 #
 def button_click():
     print("Button clicked!")
     
-
 #
 root = tk.Tk()
 root.title("Button Example")
+
+#
+button = tk.Button(root,text="Click Me!", command=button_click)
+button.pack()
+
+#
+root.mainloop()
+
