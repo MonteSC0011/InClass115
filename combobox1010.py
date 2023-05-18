889e31df60165b1b382a62b476e6c0a600ea2135
SC0011 <monte.faison@seattlecolleges.edu>
2023-05-18 12:06:32 -0700

    1010 class

--- combobox1010.py
+++ combobox1010.py
@@ -7,4 +7,10 @@ from tkinter import ttk
 def on_select(event):
     
     selected_item = event.widget.get()
+    print("Selected item:", selected_item)
+    
+#
+root = tk.Tk()
+root.title("Combobox Example")
+    
 
