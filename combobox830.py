c961b9c0eb03a510b8f9b77a6cc043da7a37ce98
SC0011 <monte.faison@seattlecolleges.edu>
2023-05-18 09:32:41 -0700

    830 class

--- combobox830.py
+++ combobox830.py
@@ -1,2 +1,15 @@
 #
 import tkinter as tk
+from tkinter import ttk
+
+#
+def on_select(event):
+    
+    #Create an item object that obtains the value of the selected item. 
+    selected_item = event.widget.get()
+    print("Selected item:", selected_item)
+  
+#    
+root = tk.Tk()
+root.title("Monte is silly")
+
