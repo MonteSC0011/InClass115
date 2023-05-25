6a29736c60643d67a8fccffd1d1b4e9320e9ef4a
SC0011 <monte.faison@seattlecolleges.edu>
2023-05-25 10:23:25 -0700

    1010 class

--- combobox1010.py
+++ combobox1010.py
@@ -12,5 +12,13 @@ def on_select(event):
 #
 root = tk.Tk()
 root.title("Combobox Example")
+
+#
+items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+
+#
+combo_box = ttk.Combobox(root, values=items)
+combo_box.bind("<<ComboboxSelected>>", on_select)
+
     
 
